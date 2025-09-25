29615713e499eb68297c8b73611b9321964c4803
shiva <udayveerdiwake2@r@gmailcom>
2025-09-25 18:35:30 +0530

    merter

--- Unit _Converter.c
+++ Unit _Converter.c
@@ -5,8 +5,9 @@ int main() {
     float km, m, cm;
 
     printf("1. Kilometer to Meter\n");
-    printf("2. Meter to Centimeter\n");
-    printf("3. Centimeter to Meter\n");
+    printf("2. Meter to Kilometer\n");
+    printf("3. Meter to Centimeter\n");
+    printf("4. Centimeter to Meter\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
 
@@ -18,12 +19,18 @@ int main() {
             printf("Meters = %.2f\n", m);
             break;
         case 2:
-            printf("Enter meters: ");
-            scanf("%f", &m);
-            cm = m * 100;
-            printf("Centimeters = %.2f\n", cm);
+             printf("Enter meters :");
+             scanf("%f", &m);
+            km = m / 1000;
+            printf("Kilometers = %.2f\n", km);
             break;
         case 3:
+            printf("Enter centimeters: ");
+            scanf("%f", &cm);
+            m = cm / 100;
+            printf("Meters = %.2f\n", m);
+            break;
+        case 4:
             printf("Enter centimeters: ");
             scanf("%f", &cm);
             m = cm / 100;
