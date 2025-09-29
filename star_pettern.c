c937ddc876cb220690b9af2bb29be9b16b0d630b
shiva <udayveerdiwake2@r@gmailcom>
2025-09-29 19:54:31 +0530

    yes

--- star_pettern.c
+++ star_pettern.c
@@ -1,21 +1,65 @@
 #include <stdio.h>
 int main() {
-int i, j, n;
+
+    // Simple Right Triangle
+// int i, j, n;
 // printf("Enter number of rows: ");
 // scanf("%d", &n);
 // for(i = 1; i <= n; i++){
+
 //     for(j = 1; j<=i; j++){
 //     printf("* ");
 //     }
 //     printf("\n");
 // }
 
+// Inverted Right Triangle
+// printf("Enter number of rows: ");
+// scanf("%d", &n);
+// for(i = n; i >= 1; i--){
+//     for(j = 1; j<=i; j++){
+//     printf("* ");
+//     }
+//     printf("\n");
+// }
 
+// Pyramid Pattern
+int i, j, space, n;
 printf("Enter number of rows: ");
 scanf("%d", &n);
-for(i = n; i >= 1; i--){
-    for(j = 1; j<=i; j++){
-    printf("* ");
+for(i = 1; i <= n; i++){
+    for(space = 1; space <= n-i; space++){
+        printf(" ");
+    }
+    for(j = 1; j <= (2 * i - 1); j++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+// Diamond Pattern
+// printf("Enter number of rows: ");
+// scanf("%d", &n);
+// for(i = n; i >= 1; i--){
+//     for(space = 1; space <= n - i; space++){
+//         printf(" ");
+//     }
+//     for(j = 1; j <= (2 * i - 1); j++){
+//         printf("*");
+//     }
+//     printf("\n");
+
+// }
+
+// Diamond Pattern
+// printf("Enter number of rows: ");
+// scanf("%d", &n);
+for(i = n - 1; i >= 1; i--){
+    for(space = 1; space <= n - i; space++){
+        printf(" ");
+    }
+    for(j = 1; j <= (2 * i - 1); j++){
+        printf("*");
     }
     printf("\n");
 }
