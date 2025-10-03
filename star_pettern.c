e2138697c4eaa4a3e40f45aedfe27816acedf351
shiva <udayveerdiwake2@r@gmailcom>
2025-10-03 17:06:30 +0530

    square

--- star_pettern.c
+++ star_pettern.c
@@ -25,17 +25,17 @@ int main() {
 
 // Pyramid Pattern
 int i, j, space, n;
-printf("Enter number of rows: ");
-scanf("%d", &n);
-for(i = 1; i <= n; i++){
-    for(space = 1; space <= n-i; space++){
-        printf(" ");
-    }
-    for(j = 1; j <= (2 * i - 1); j++){
-        printf("*");
-    }
-    printf("\n");
-}
+// printf("Enter number of rows: ");
+// scanf("%d", &n);
+// for(i = 1; i <= n; i++){
+//     for(space = 1; space <= n-i; space++){
+//         printf(" ");
+//     }
+//     for(j = 1; j <= (2 * i - 1); j++){
+//         printf("*");
+//     }
+//     printf("\n");
+// }
 
 // Diamond Pattern
 // printf("Enter number of rows: ");
@@ -54,16 +54,32 @@ for(i = 1; i <= n; i++){
 // Diamond Pattern
 // printf("Enter number of rows: ");
 // scanf("%d", &n);
-for(i = n - 1; i >= 1; i--){
-    for(space = 1; space <= n - i; space++){
-        printf(" ");
-    }
-    for(j = 1; j <= (2 * i - 1); j++){
-        printf("*");
+
+// for(i = n - 1; i >= 1; i--){
+//     for(space = 1; space <= n - i; space++){
+//         printf(" ");
+//     }
+//     for(j = 1; j <= (2 * i - 1); j++){
+//         printf("*");
+//     }
+//     printf("\n");
+// }
+
+
+// Hollow Square Pattern
+printf("Enter size of square: ");
+scanf("%d", &n);
+for(i = 1; i <= n; i++){
+    for(j = 1; j <= n; j++){
+        if(i == 1 || i == n || j == 1 || j == n){
+            printf("* ");
+        }
+        else{
+            printf("  ");
+        }
     }
     printf("\n");
 }
-
 return 0;
 
 }
