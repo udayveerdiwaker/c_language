b5c578f4faac470777e2c9ce52ac9d6e21312bc4
shiva <udayveerdiwake2@r@gmailcom>
2025-10-31 17:23:23 +0530

    star

--- star_pettern.c
+++ star_pettern.c
@@ -1,137 +1,140 @@
 #include <stdio.h>
+
 int main() {
 
+    int i, j, space, n;
+
+    // ------------------------------
     // Simple Right Triangle
-// int
-int i, j, space, n;
-
-
-// printf("Enter number of rows: ");
-// scanf("%d", &n);
-// for(i = 1; i <= n; i++){
-//     for(j = 1; j<=i; j++){
-//     printf("* ");
-//     }
-//     printf("\n");
-// }
-// Inverted Right Triangle
-// printf("Enter number of rows: ");
-// scanf("%d", &n);
-// for(i = n; i >= 1; i--){
-//     for(j = 1; j<=i; j++){
-//     printf("* ");
-//     }
-//     printf("\n");
-// }
-
-// Pyramid Pattern
-<<<<<<< Updated upstream
-int i, j, space, n;
-=======
->>>>>>> Stashed changes
-// printf("Enter number of rows: ");
-// scanf("%d", &n);
-// for(i = 1; i <= n; i++){
-//     for(space = 1; space <= n-i; space++){
-//         printf(" ");
-//     }
-//     for(j = 1; j <= (2 * i - 1); j++){
-//         printf("*");
-//     }
-//     printf("\n");
-// }
-
-// Diamond Pattern
-// printf("Enter number of rows: ");
-// scanf("%d", &n);
-// for(i = n; i >= 1; i--){
-//     for(space = 1; space <= n - i; space++){
-//         printf(" ");
-//     }
-//     for(j = 1; j <= (2 * i - 1); j++){
-//         printf("*");
-//     }
-//     printf("\n");
-
-// }
-
-// Diamond Pattern
-
-// printf("Enter number of rows: ");
-// scanf("%d", &n);
-<<<<<<< Updated upstream
-
-// for(i = n - 1; i >= 1; i--){
-=======
-// for(i = 1; i <= n; i++){
-//     for(space = 1; space <= n - i; space++){
-//         printf(" ");
-//     }
-//     for(j = 1; j <= (2 * i - 1); j++){
-//         printf("*");
-//     }
-//     printf("\n");
-// }for(i = n - 1; i >= 1; i--){
->>>>>>> Stashed changes
-//     for(space = 1; space <= n - i; space++){
-//         printf(" ");
-//     }
-//     for(j = 1; j <= (2 * i - 1); j++){
-//         printf("*");
-//     }
-//     printf("\n");
-// }
-
-
-<<<<<<< Updated upstream
-// Hollow Square Pattern
-printf("Enter size of square: ");
-scanf("%d", &n);
-for(i = 1; i <= n; i++){
-    for(j = 1; j <= n; j++){
-        if(i == 1 || i == n || j == 1 || j == n){
+    // ------------------------------
+    /*
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++){
             printf("* ");
         }
-        else{
-            printf("  ");
+        printf("\n");
+    }
+    */
+
+    // ------------------------------
+    // Inverted Right Triangle
+    // ------------------------------
+    /*
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i = n; i >= 1; i--){
+        for(j = 1; j <= i; j++){
+            printf("* ");
         }
+        printf("\n");
     }
-    printf("\n");
-}
-=======
-printf("Enter number of rows: ");
-scanf("%d", &n);
-for(i = 1; i <= n; i++){
-    for(space = n; space > i; space--){
-        printf(" ");
+    */
+
+    // ------------------------------
+    // Pyramid Pattern
+    // ------------------------------
+    /*
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        for(space = 1; space <= n - i; space++){
+            printf(" ");
+        }
+        for(j = 1; j <= (2 * i - 1); j++){
+            printf("*");
+        }
+        printf("\n");
     }
-    for(j = 1; j < 2 * i; j++){
-        if(j%2==0)
-        printf("0");
-        else
-        printf("1");
+    */
+
+    // ------------------------------
+    // Diamond Pattern
+    // ------------------------------
+    /*
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    // Upper part of diamond
+    for(i = 1; i <= n; i++){
+        for(space = 1; space <= n - i; space++){
+            printf(" ");
+        }
+        for(j = 1; j <= (2 * i - 1); j++){
+            printf("*");
+        }
+        printf("\n");
     }
-    printf("\n");
-}
 
+    // Lower part of diamond
+    for(i = n - 1; i >= 1; i--){
+        for(space = 1; space <= n - i; space++){
+            printf(" ");
+        }
+        for(j = 1; j <= (2 * i - 1); j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    */
 
-// printf("Enter number of rows: ");
-// scanf("%d", &n);
+    // ------------------------------
+    // Hollow Square Pattern
+    // ------------------------------
+    printf("Enter size of square: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            if(i == 1 || i == n || j == 1 || j == n){
+                printf("* ");
+            }
+            else{
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
 
-// for(i = 1; i <= n; i++){
-//     for(space = n; space > i; space--){
-//         printf(" ");
-//     }
-//     for(j = 1; j <= i; j++){
-//        printf("%d", j);
-//     }
-//     for(j = i - 1; j >= 1; j--){
-//        printf("%d", j);
-//     }
-//     printf("\n");
-// }
+    // ------------------------------
+    // Optional: 1-0 Pyramid Pattern
+    // ------------------------------
+    /*
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        for(space = n; space > i; space--){
+            printf(" ");
+        }
+        for(j = 1; j < 2 * i; j++){
+            if(j % 2 == 0)
+                printf("0");
+            else
+                printf("1");
+        }
+        printf("\n");
+    }
+    */
 
->>>>>>> Stashed changes
-return 0;
+    // ------------------------------
+    // Number Pyramid Pattern
+    // ------------------------------
+    /*
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        for(space = n; space > i; space--){
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++){
+            printf("%d", j);
+        }
+        for(j = i - 1; j >= 1; j--){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    */
 
+    return 0;
 }
