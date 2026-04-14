39a756a33c71565def87da821bd74c5c16dd13f8
shiva <udayveerdiwake2@r@gmailcom>
2026-04-14 20:01:35 +0530

    cch
    chh

---
+++ C++_language/class.cpp
@@ -0,0 +1,115 @@
+// #include<iostream>
+// using namespace std;
+
+// class Student {
+// public:
+//     int id;
+//     string name;
+
+//     void display() {
+//         cout << "ID: " << id << endl;
+//         cout << "Name: " << name << endl;
+//     }
+// };
+
+
+// int main() {
+//     Student s1;
+
+//     s1.id = 101;
+//     s1.name = "Rahul";
+
+
+//     s1.display();
+
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// class Student {
+// public:
+//     int id;
+
+//     Student() {
+//         id = 100;
+//     }
+
+//     void show() {
+//         cout << id;
+//     }
+// };
+
+// int main() {
+//     Student s1;
+//     s1.show();
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// class Student {
+// public:
+//     int id;
+
+//     void setData(int i) {
+//         id = i;
+//     }
+
+//     void show() {
+//         cout << id << endl;
+//     }
+// };
+
+
+// int main() {
+//     Student s1, s2, s3;
+
+    
+//     s1.setData(101);
+//     s2.setData(102);
+// s3.setData(10256156);
+
+//     s1.show();
+//     s2.show();
+// s3.show();
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// class Student {
+// private:
+//     int id;
+
+// public:
+//     void setData(int i) {
+//         id = i;
+//     }
+
+//     void show() {
+//         cout << id;
+//     }
+// };
+
+// int main() {
+//     Student s1;
+//     s1.setData(200);
+//     s1.show();
+//     return 0;
+// }
+
+#include<iostream>
+using namespace;
+
+int main() {
+    int a, b, sum;
+    cin >> a >> b;
+    sum = a + b;
+    cout << sum;
+    return 0;
+}
